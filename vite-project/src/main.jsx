fb06889d689b09be63fb77fe945c4e1bb35a1a96
abhishek <aabhiojha08@Outlook.com>
2026-05-18 13:24:27 +0545

    added another comment

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
-
-        {/*yoyoy this is the comment*/}
+        {/* this is the comment*/}
+        {/*this is the another comment*/}
     </React.StrictMode>
 )
